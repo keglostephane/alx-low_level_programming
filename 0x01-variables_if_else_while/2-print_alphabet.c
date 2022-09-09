355612bd5970745117ed157adbac747c7c3101ee
Stephane Keglo <stephanekeglo@gmail.com>
2022-09-09 14:04:16 -0700

    add 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Start the program
+ *
+ * Return: O(success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
