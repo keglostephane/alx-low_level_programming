7e3c80dfb705f152d40164b3a9c9f33384d51829
Stephane Keglo <stephanekeglo@gmail.com>
2022-09-09 18:51:01 -0700

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Start the program
+ *
+ * Return: O(success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
