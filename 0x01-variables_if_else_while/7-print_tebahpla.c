a5040dab7f874ead27efcafe00c479f6783d0c79
Stephane Keglo <stephanekeglo@gmail.com>
2022-09-09 19:24:56 -0700

    add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Start the program
+ *
+ * Return: O(success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
