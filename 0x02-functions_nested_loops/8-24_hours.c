f1c78e544aa03c686a6511142ef4fad25d55d656
Stephane Keglo <stephanekeglo@gmail.com>
2022-09-14 22:26:36 -0700

    add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every of the day
+ *
+ * Description : Print every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 4)
+				break;
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
