5d579fa33af6b7667ff1cf06eaca58ee66f0be2a
Stephane Keglo <stephanekeglo@gmail.com>
2022-09-19 22:23:17 -0700

    add README.md, 0-reset_to_98.c, main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a given argument to 98
+ * @n: int pointer - point to given argument
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05. C - Pointers, arrays and strings
+
+A series of C projects related to Pointers, Arrays and Strings.
