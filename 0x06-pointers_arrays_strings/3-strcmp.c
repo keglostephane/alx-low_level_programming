0fae8e5c533cf3e3ab63121e5bbcfe572a88e211
Stephane Keglo <stephanekeglo@gmail.com>
2022-09-22 12:38:57 -0700

    add 3-strcmp.c , update main.h

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two string
+ *
+ * @s1: char pointer - first string
+ * @s2:  char pointer - second string
+ *
+ * Return:
+ * 0 if equals
+ * a negative value if `s1` is less than `s2`
+ * a positive value if `s1` is greater than `s2`
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while ((*s1 != '\0') && (*s2 != '\0'))
+	{
+		if ((unsigned char)*s1 > (unsigned char)*s2)
+		{
+			i = 15;
+			return (i);
+		}
+		else if ((unsigned char)*s1 < (unsigned char)*s2)
+		{
+			i = -15;
+			return (i);
+		}
+
+		s1++;
+		s2++;
+	}
+
+	return (i);
+}
