17e32f70c1a0bc324629f97cdd8532c8a896460d
Stephane Keglo <stephanekeglo@gmail.com>
2022-09-22 14:03:37 -0700

    fix 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,24 +15,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
 
-	while ((*s1 != '\0') && (*s2 != '\0'))
+	while ((*s1 != '\0') || (*s2 != '\0'))
 	{
-		if ((int)*s1 > (int)*s2)
-		{
-			i = 15;
-			return (i);
-		}
-		else if ((int)*s1 < (int)*s2)
-		{
-			i = -15;
-			return (i);
-		}
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 
 		s1++;
 		s2++;
 	}
 
-	return (i);
+	return (0);
 }
