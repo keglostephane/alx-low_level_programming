d4b8e13999cc27acfdea2fc79ca9a11342547774
Stephane Keglo <stephanekeglo@gmail.com>
2022-09-26 17:52:21 -0700

    add 3-strspn.c , update main.h

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,43 @@
+/**
+ * _strspn -  gets the length of a prefix substring
+ *
+ * @s: char pointer - string
+ *
+ * @accept: char pointer - substring
+ *
+ * Return: the number of bytes in `s` which consist only of bytes
+ * from `accept`
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+
+	char *str = s;
+
+	while (*accept != '\0')
+	{
+		while (*s != '\0')
+		{
+			if (*accept == *s)
+			{
+				c++;
+				break;
+			}
+
+			s++;
+		}
+
+		accept++;
+
+		s = str;
+	}
+
+	if (c != 0)
+		c++;
+
+	return (c);
+}
+
+
