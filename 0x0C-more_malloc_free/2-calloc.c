820b49f68add32eeaf7a989fd76836d8a6de858e
Stephane Keglo <stephanekeglo@gmail.com>
2022-10-07 08:09:14 -0700

    fix 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * sizeof(size) + 1);
+	ptr = malloc(nmemb * sizeof(size) * sizeof(ptr));
 
 	if (!ptr)
 		return (NULL);
