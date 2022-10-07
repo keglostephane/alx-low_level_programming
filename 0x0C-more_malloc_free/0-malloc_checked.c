31a34b063c71f57b193573013a8d72dd329c38ff
Stephane Keglo <stephanekeglo@gmail.com>
2022-10-06 18:09:56 -0700

    add README.md , main.h , 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: unsigned int - number of bytes memory to allocate
+ *
+ * Return: a pointer to the allocated memory
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+# 0x0C. C - More malloc, free
+
+More C projects on dynamic memory allocation using `malloc`, `free`, `calloc`, `realloc`.
