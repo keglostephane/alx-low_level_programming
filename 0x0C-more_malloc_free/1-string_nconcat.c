9a890d7ea30c4eb4ade29e960051f48bee8c303d
Stephane Keglo <stephanekeglo@gmail.com>
2022-10-07 21:56:41 -0700

    fix 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -52,7 +52,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= l2)
 		n = l2;
 
-	ptr = malloc(l1 + n * sizeof(ptr));
+	ptr = malloc(l1 + n + 1);
 
 	if (!ptr)
 		return (NULL);
