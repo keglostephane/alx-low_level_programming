2cc2213e1e367304cbeb11d645c93e98e41c7b37
Stephane Keglo <stephanekeglo@gmail.com>
2022-10-12 20:52:50 -0700

    improve 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -19,7 +19,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	void (*fptr)(int);
 
-	if (array == NULL || size == 0 || action == NULL)
+	if (array == NULL || size <= 0 || action == NULL)
 		return;
 
 	fptr = action;
