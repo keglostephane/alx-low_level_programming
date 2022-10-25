850aa54c30fff1b66f0b51badd6d4eead1c212b9
Stephane Keglo <stephanekeglo@gmail.com>
2022-10-25 13:51:38 -0700

    add 5-free_listint2.c , update lists.h

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ *
+ * @head: pointer to listint_t pointer - points to head of the list
+ *
+ * Description: frees a list and sets the head to NULL
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr_node;
+
+	curr_node = *head;
+
+	while (curr_node != NULL)
+	{
+		*head = (*head)->next;
+		free(curr_node);
+		curr_node = *head;
+	}
+
+	*head = NULL;
+}
