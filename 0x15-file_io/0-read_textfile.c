6d62e2ed143be1d816c07cbdfab2ad9299887a86
Stephane Keglo <stephanekeglo@gmail.com>
2022-11-05 08:53:04 -0700

    fix 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -38,9 +38,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	rc = read(fd, buffer, letters);
 
 	if (rc == -1)
+	{
 		return (0);
+		free(buffer);
+	}
 
 	wc = write(STDIN_FILENO, buffer, rc);
+
+	if (wc != rc)
+	{
+		return (0);
+		free(buffer);
+	}
 
 	close(fd);
 
