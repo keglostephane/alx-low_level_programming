7e6e50b49725cbbef89a8f4e659b4c367a9b89f8
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-14 02:42:55 +0000

    Create 6-size.c C file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 in success, -1 in failure
+ *
+ */
+int main(void)
+{
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
