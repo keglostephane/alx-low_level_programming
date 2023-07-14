a8e6cea3aa575b44a7904f29d9fa49562e749dd9
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-14 02:20:25 +0000

    Create 5-printf.c C file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 in success, -1 in failure
+ *
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
