01ce75346c514878d2b9ad9ba10e0a58331d84b7
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-14 02:14:25 +0000

    Create 4-puts.c C file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 in success, -1 in failure
+ *
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
