a82132fe56ad78b570f15bfd9541aadc48648cf4
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-15 01:23:12 +0000

    Create 9-print_comb.c C file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 in success
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
