e09dc23aeb120f16513819f9f7a7edadfc4f2d15
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-20 16:31:55 +0000

    Fix 5-more_numbers.c file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * print_two_digits - prints digits for positive numbers less than 100
- *
- * @n : number
- */
-void print_two_digits(int n)
-{
-	if (n >= 0 && n < 100)
-	{
-		if (n < 10)
-			_putchar(n + '0');
-		else
-		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-		}
-	}
-}
-
-/**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  *
  */
@@ -30,8 +11,13 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			print_two_digits(j);
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
 
-		putchar('\n');
+			_putchar(j % 10 + '0');
+		}
+
+		_putchar('\n');
 	}
 }
