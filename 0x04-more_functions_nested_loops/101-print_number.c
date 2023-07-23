8462f94b2ba0f86007ecf0d8c222858ab5dd0971
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-23 17:50:31 +0000

    Fix 101-print_number.c #1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,24 +10,28 @@ void print_number(int n)
 {
 	int r;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
 	if  (n == 0)
 	{
 		_putchar(n + '0');
 		return;
 	}
+
+	if (n < 0)
+		_putchar('-');
+
 	if (n >= 1 && n <= 9)
 	{
+
 		_putchar(n + '0');
 		return;
 	}
 
-	r = n % 10;
-	print_number(n / 10);
+	r = (n % 10 > 0) ? n % 10 : -(n % 10);
+
+	if (n < 0)
+		print_number(n / -10);
+	else
+		print_number(n / 10);
+
 	_putchar(r + '0');
 }
