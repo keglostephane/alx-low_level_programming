8cdc762269e8f4f2c09d853771073822592716d4
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-25 01:11:39 +0000

    Create 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ *
+ * @s: pointer to the start of the string
+ *
+ */
+void print_rev(char *s)
+{
+	char *str = s;
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+
+	while (i != -1)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+
+	_putchar('\n');
+}
