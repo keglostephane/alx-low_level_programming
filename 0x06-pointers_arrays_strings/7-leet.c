56cc010d27bfdb71efad87ac23ee7514e3a15314
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-26 23:55:47 +0000

    Fix 7-leet.c #1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,21 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	int lower[5] = {97, 101, 111, 116, 108};
+	int upper[5] = {65, 69, 79, 84, 76};
+	int values[5] = {52, 51, 48, 55, 49};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 65 || s[i] == 92)
-			s[i] = 52;
-		else if (s[i] == 69 || s[i] == 101)
-			s[i] = 51;
-		else if (s[i] == 79 || s[i] == 111)
-			s[i] = 48;
-		else if (s[i] == 84 || s[i] == 116)
-			s[i] = 55;
-		else if (s[i] == 76 || s[i] == 108)
-			s[i] = 49;
+		if (s[i] == 65 || s[i] == 97 || s[i] == 69 || s[i] == 101 ||
+		    s[i] == 79 || s[i] == 111 || s[i] == 84 || s[i] == 116 ||
+		    s[i] == 76 || s[i] == 108)
+		{
+			for (j = 0; j <= 4; j++)
+				if (upper[j] == s[i] || lower[j] == s[i])
+					s[i] = values[j];
+		}
 	}
 
 	return (s);
