972b7fbfdfff40771813bbb23ca6b57f430890a0
Dodzi Keglo <stephanekeglo@gmail.com>
2023-07-27 00:02:03 +0000

    Fix 7-leet.c #2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,13 +15,10 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 65 || s[i] == 97 || s[i] == 69 || s[i] == 101 ||
-		    s[i] == 79 || s[i] == 111 || s[i] == 84 || s[i] == 116 ||
-		    s[i] == 76 || s[i] == 108)
+		for (j = 0; j <= 4; j++)
 		{
-			for (j = 0; j <= 4; j++)
-				if (upper[j] == s[i] || lower[j] == s[i])
-					s[i] = values[j];
+			if (s[i] == lower[j] || s[i] == upper[j])
+				s[i] = values[j];
 		}
 	}
 
