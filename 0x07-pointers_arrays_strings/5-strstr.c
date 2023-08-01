cd0b81e89f02d2bc2d9ca7a4e51ae0657ea16131
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-01 02:04:29 +0000

    Create 5-strstr.c file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+/**
+ * _strstr - locate a substring in a string
+ *
+ * @haystack: pointer to the string to be scanned for a substring
+ *
+ * @needle: pointer to the substring to search
+ *
+ * Return: a pointer to the first occurence of the substring in the string,
+ * else NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, pos;
+
+
+	for (i = 0, j = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[j])
+		{
+			pos = i;
+
+			while (needle[j] != '\0')
+			{
+				if (haystack[i] == needle[j])
+					i++;
+				else
+				{
+					break;
+					j = 0;
+				}
+
+				j++;
+			}
+
+			if (j != 0)
+				return (haystack + pos);
+		}
+	}
+
+	return (NULL);
+}
