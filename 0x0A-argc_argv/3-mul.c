4cd7b74e52f4de6ed77931e1359174ca9360ddec
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-05 01:10:00 +0000

    Create 3-mul.c file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: number of arguments
+ *
+ * @argv: list of arguments
+ *
+ * Return: 0 Always success
+ *
+ */
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
