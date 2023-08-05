98b7801c5a7ca5a0ffdaf792920057517190f03a
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-05 01:38:06 +0000

    Create 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers passed as arguments
+ *
+ * @argc: number of arguments
+ *
+ * @argv: list of arguments
+ *
+ * Return: O in Success, 1 if one of the number contains symbols
+ * that are not digits
+ *
+ */
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]))
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
