9d313d1f0e115752ec9e9db0581845eb414d8463
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-05 00:35:47 +0000

    Create 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ *
+ * @argc: number of arguments
+ *
+ * @argv: list of arguments
+ *
+ * Return: 0 Always success
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
