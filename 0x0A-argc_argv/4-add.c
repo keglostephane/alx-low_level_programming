414407d2e3adc6a62a6ae00c5e0b3c1e24f2ca4c
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-05 02:30:22 +0000

    Fix 4-add.c #3

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - adds positive numbers passed as arguments
@@ -14,17 +15,18 @@
  */
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) || *argv[i] == '0')
-			sum += atoi(argv[i]);
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
