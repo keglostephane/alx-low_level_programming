37254b2b128b57cdce02e24d2396582f7b462ca0
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-08 19:49:50 +0000

    Fix 0-create_array.c #2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	if (!size)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * size);
+	ptr = malloc(sizeof(int) * (size - 1));
 
 	if (!ptr)
 		return (NULL);
