44ca6a16fd674f2b5d9477795c634b58b11b5d00
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-09 00:40:53 +0000

    Free memory allocation when malloc failed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,14 +25,20 @@ int **alloc_grid(int width, int height)
 	ptr = malloc(sizeof(int *) * height);
 
 	if (!ptr)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		ptr[i] = malloc(sizeof(int) * width);
 
 		if (!ptr[i])
+		{
+			free(ptr[i]);
 			return (NULL);
+		}
 	}
 
 
