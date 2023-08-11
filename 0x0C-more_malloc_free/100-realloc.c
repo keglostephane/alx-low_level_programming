273c449f8125021edc1aab3835758b4c9f88001c
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-11 21:51:56 +0000

    Fix logic of the code

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,22 +30,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (!ptr)
 			return (NULL);
 	}
-
-	if (!new_size)
+	else
 	{
-		free(ptr);
-		return (NULL);
-	}
-
-	if (new_size == old_size)
-		return (ptr);
-
-	if (new_size > old_size)
-	{
-		ptr = malloc(new_size);
-
-		if (!ptr)
+		if (!new_size)
+		{
+			free(ptr);
 			return (NULL);
+		}
+
+		if (new_size == old_size)
+			return (ptr);
+
+		if (new_size > old_size)
+		{
+			ptr = malloc(new_size);
+
+			if (!ptr)
+				return (NULL);
+		}
 	}
 
 	return (ptr);
