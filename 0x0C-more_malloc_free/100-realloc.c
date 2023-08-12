e36328d3157726061856a30728cf22cd11dfd415
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-12 00:53:49 +0000

    Fix 100-realloc.c #2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,9 +23,6 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *nptr;
-	unsigned int i, nb;
-
-	nb = old_size < new_size ? old_size : new_size;
 
 	if (!ptr)
 	{
@@ -51,9 +48,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 			if (!nptr)
 				return (NULL);
-
-			for (i = 0; i < nb; i++)
-				nptr[i] = ((char *)ptr)[i];
 
 			return (nptr);
 		}
