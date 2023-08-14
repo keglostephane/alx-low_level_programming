e085aff7b7aec3c3e7ec8a70d929479c31121f61
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-14 18:20:55 +0000

    Create 1-init_dog.c file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a dog
+ *
+ * @d: pointer to structure dog
+ *
+ * @name: dog's name
+ *
+ * @age: dog's age
+ *
+ * @owner: dog's owner name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
