38d4a0b18f19ad30eae6fb475b9b90486be51e35
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-14 23:18:08 +0000

    Fix typos in 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name : (nil)\n");
+			printf("Name: (nil)\n");
 
 		if (d->age)
 			printf("Age: %f\n", d->age);
