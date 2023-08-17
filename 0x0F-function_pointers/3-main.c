8334f04897f21e35c822eadfc3dba71b3b238ff7
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-17 02:11:17 +0000

    Fix 3-main.c #3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@
 int main(int argc, char **argv)
 {
 	int a, b;
-	char *op;
+	char *operator;
 	int (*funct)(int, int);
 	int result;
 
@@ -34,11 +34,11 @@ int main(int argc, char **argv)
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	op = argv[2];
+	operator = argv[2];
 
-	funct = get_op_func(op);
+	funct = get_op_func(operator);
 
-	if (funct == NULL || *(op + 1) != '\0')
+	if (funct == NULL || *(operator + 1) != '\0')
 	{
 		printf("Error\n");
 		exit(99);
