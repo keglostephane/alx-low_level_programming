e7097614b9a52a830d42dd8035e1ab6c53608794
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-29 00:09:22 +0000

    Create 5-free_listint2.c file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ *
+ * @head: pointer to the head of the linked list
+ *
+ * sets the head to NULL after freeing the list
+ */
+void free_listint2(listint_t **head)
+{
+	int i;
+	listint_t *curr_node, *next_node;
+
+	curr_node = *head;
+
+	for (i = 0; curr_node != NULL; i++)
+	{
+		next_node = curr_node->next;
+		free(curr_node);
+		curr_node = next_node;
+	}
+
+	*head = NULL;
+}
