34a213177cd2df54134b83349241f177c8bc2518
Dodzi Keglo <stephanekeglo@gmail.com>
2023-08-29 13:21:00 +0000

    Add new line

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -75,7 +75,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!new_node)
 		return (NULL);
+
 	new_node->n = n;
+
 	if (!len && idx)
 		return (NULL);
 	if (len && idx > len)
