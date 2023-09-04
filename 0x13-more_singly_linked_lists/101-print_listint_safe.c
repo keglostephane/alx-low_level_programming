041787e785ef557cd0682ef1ef8944634b794bed
Dodzi Keglo <stephanekeglo@gmail.com>
2023-09-04 19:51:07 +0000

    Create 101-print_listint_safe.c file

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ *
+ * @head: head of the linked list
+ *
+ * Return: the number of nodes in the list
+ *
+ * if the function fails, exit the program with status 98
+ *
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	unsigned int count, cycle;
+
+	if (!head)
+		exit(98);
+
+	slow = head;
+	fast = head;
+	cycle = 0;
+	while (slow != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+
+		if (!cycle)
+		{
+			slow = slow->next;
+			if (fast && fast->next)
+				fast = fast->next->next;
+			if (fast == slow)
+			{
+				cycle = 1;
+				fast = head;
+			}
+		}
+		else
+		{
+			if (slow == fast)
+				break;
+			slow = slow->next;
+			fast = fast->next;
+		}
+	}
+
+	return (count);
+}
