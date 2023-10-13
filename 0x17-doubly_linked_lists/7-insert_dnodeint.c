a9b48f145dfb0cfe6d319405c5911f1e011d253e
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-13 02:34:24 +0000

    Fix 7-insert_dnodeint.c #5

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,56 +3,52 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: dlistint_t double pointer - head of doubly linked list
+ * @idx: unsigned int - index to insert to new node to
+ * @n: int - data of node to insert
  *
- * @h: head of the doubly linked list
+ * Return: the address of the new node or NULL
  *
- * @idx: index of the new node to insert
- *
- * @n: value of the new node
- *
- * Return: the address of the new node, or NULL if it failed.
- *
- * If it's not possible to add the new node at index `idx`,
- * do not add the new node and return NULL
+ * If it is not possible to add the new node at index idx, do not add the node
+ * and return NULL
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *new_node, *curr, *prev;
+	unsigned int index = 0;
+	dlistint_t *new_node, *node, *prev;
 
+	prev = NULL;
+	node = *h;
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-
 	new_node->n = n;
-	curr = *h;
-	i = 0;
-	while (curr)
+	while (node)
 	{
-		if (idx == i)
+		if (index == idx)
 		{
-			prev = curr->prev;
+			prev = node->prev;
 			break;
 		}
-		curr = curr->next;
-		++i;
+		node = node->next;
+		index++;
 	}
-	if (!curr && idx)
+	if (!node && idx != 0)
 		return (NULL);
-	if (idx != i)
+	if (index != idx)
 		return (NULL);
 	if (!idx)
 	{
+		new_node->next = node;
 		new_node->prev = NULL;
-		new_node->next = curr;
-		if (curr)
-			curr->prev = new_node;
+		if (node)
+			node->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
 	new_node->next = prev->next;
 	prev->next->prev = new_node;
-	prev->next = new_node;
 	new_node->prev = prev;
+	prev->next = new_node;
 	return (new_node);
 }
