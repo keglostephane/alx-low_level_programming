db2ce5262a184bee7b1fde113c4a3306fa0b683a
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-13 01:13:34 +0000

    Create 7-insert_dnodeint.c file

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ *
+ * @h: head of the doubly linked list
+ *
+ * @idx: index of the new node to insert
+ *
+ * @n: value of the new node
+ *
+ * Return: the address of the new node, or NULL if it failed.
+ *
+ * If it's not possible to add the new node at index `idx`,
+ * do not add the new node and return NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i, len;
+	dlistint_t *new_node, *curr, *prev;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!h || !*h || !new_node)
+		return (NULL);
+
+	curr = *h;
+	len = 0;
+	while (curr)
+	{
+		curr = curr->next;
+		++len;
+	}
+
+	new_node->n = n;
+	curr = *h;
+	i = 0;
+	while (curr && i < len && i != idx)
+	{
+		prev = curr;
+		curr = curr->next;
+		++i;
+	}
+
+	if (idx == i)
+	{
+		if (idx)
+		{
+			new_node->next = prev->next;
+			prev->next->prev = new_node;
+			prev->next = new_node;
+			new_node->prev = prev;
+		}
+		if (!idx)
+		{
+			new_node->prev = NULL;
+			new_node->next = *h;
+			(*h)->prev = new_node;
+		}
+		return (new_node);
+	}
+	return (NULL);
+}
