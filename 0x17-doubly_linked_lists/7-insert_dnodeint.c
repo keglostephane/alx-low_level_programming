330c6b967cde2c6ba06e2a3131c632c7f71f8309
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-13 20:31:24 +0000

    Fix 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,8 +3,11 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
+ *
  * @h: dlistint_t double pointer - head of doubly linked list
+ *
  * @idx: unsigned int - index to insert to new node to
+ *
  * @n: int - data of node to insert
  *
  * Return: the address of the new node or NULL
@@ -14,40 +17,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int len = 0, i = 0;
-	dlistint_t *new_node, *curr, *prev;
+	unsigned int index;
+	dlistint_t *new_node, *curr;
 
 	new_node = malloc(sizeof(dlistint_t));
+
 	if (!h || !new_node)
 		return (NULL);
 
-	curr = *h;
-	while (curr)
-	{
-		curr = curr->next;
-		++len;
-	}
+	if (!*h && idx)
+		return (NULL);
 
 	new_node->n = n;
-	curr = *h;
-	while (curr && i < len && i != idx)
-	{
-		prev = curr;
-		curr = curr->next;
-		++i;
-	}
+
 	if (!idx)
 	{
 		new_node->prev = NULL;
-		new_node->next = curr;
-		if (curr)
-			curr->prev = new_node;
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
 		*h = new_node;
-		return (new_node);
+		return (*h);
 	}
-	new_node->next = prev->next;
-	prev->next->prev = new_node;
-	prev->next = new_node;
-	new_node->prev = prev;
+
+	for (index = 0, curr = *h; curr && index != idx; index++)
+		curr = curr->next;
+
+	if (!curr)
+		return (NULL);
+
+	new_node->next = curr;
+	new_node->prev = curr->prev;
+	curr->prev->next = new_node;
+	curr->prev = new_node;
+
 	return (new_node);
 }
