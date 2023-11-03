5e496eb13d80998d2d90a10b6f009315abe29ee4
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-03 23:22:41 +0000

    Add hash_table_print() definition

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table
+ *
+ * @ht: hash table to print
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *head;
+	unsigned long int i;
+	int found = 0;	/* detect if a key/value has been found */
+
+	printf("{");
+	for (i = 0;  ht && i < ht->size; i++)
+	{
+		head = ht->array[i];
+
+		while (head)
+		{
+			if (found)
+				printf(", ");
+			printf("\'%s\': \'%s\'", head->key, head->value);
+
+			found = 1;
+			head = head->next;
+		}
+	}
+	printf("}\n");
+}
