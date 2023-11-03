30ecc18da86bf2eb37d08f8e8ea25d68799d06d5
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-03 17:48:02 +0000

    Fix #1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (!ht || !key || !*key || !value)
+	if (!ht || !*ht || !key || !*key || !value)
 		return (0);
 
 	node = malloc(sizeof(hash_node_t));
