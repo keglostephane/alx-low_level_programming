bf653814c42baf740a12483d75efe05c86328563
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-03 10:05:37 +0000

    Create 0-hash_table_create.c file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ *
+ * @size: size of the array
+ *
+ * Return: the newly created hash table
+ * If somthing went wrong, return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_map;
+
+	hash_map = malloc(sizeof(hash_table_t));
+
+	if (!size || !hash_map)
+		return (NULL);
+
+	return (hash_map);
+}
