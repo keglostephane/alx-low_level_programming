329d965e2c68056c1d84219a0105f1a502016f5a
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-03 21:27:23 +0000

    Handle NULL hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned int long index;
 	hash_node_t *node;
 
-	if (!key || !*key)
+	if (!ht || !key || !*key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
