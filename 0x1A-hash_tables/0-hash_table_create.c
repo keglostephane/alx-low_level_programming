278390943a7008843b375cfc1d2779dc84976433
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-03 13:33:49 +0000

    Fix 0-hash_table_create.c #5

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,17 +12,27 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_map;
+	unsigned long int i;
 
+	(void) i;
 	if (!size)
 		return (NULL);
 
-	hash_map = malloc(sizeof(hash_table_t) +
-			  sizeof(hash_node_t *) * size);
+	hash_map = malloc(sizeof(hash_table_t));
 
 	if (!hash_map)
 		return (NULL);
 
+	hash_map->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!(hash_map->array))
+	{
+		free(hash_map);
+		return (NULL);
+	}
+
 	hash_map->size = size;
+	hash_map->array = NULL;
 
 	return (hash_map);
 }
