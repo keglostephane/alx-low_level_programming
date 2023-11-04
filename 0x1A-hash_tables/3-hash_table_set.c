71f032e0a421f78089897b507259a62ef928ceda
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-04 22:11:53 +0000

    Rewrite the code and Fix memory leak

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,40 +16,40 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node, *head;
+	hash_node_t *head, *node;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+
+	/* check for an existing key */
+	while (head)
+	{
+		/* existing key found, update the value */
+		if (strcmp(head->key, key) == 0)
+		{
+			free(head->value);
+			head->value = strdup(value);
+			return (1);
+		}
+		head = head->next;
+	}
+
+	/* the key doesn't exists, create a new node with key */
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-
-	if (!ht->array[index])
-		node->next = NULL;
-	else
-	{
-		head = ht->array[index];
-		/* check for an existing key */
-		while (ht->array[index])
-		{
-			/* existing key found, update the value */
-			if (strcmp(ht->array[index]->key, key) == 0)
-			{
-				ht->array[index]->value = strdup(value);
-				free(node);
-				return (1);
-			}
-			ht->array[index] = ht->array[index]->next;
-		}
-		/* collision detected, add to the beginning */
-		node->next = head;
-	}
-
 	node->key = strdup(key);
 	node->value = strdup(value);
+	/*
+	 * if no collision, node->next points to NULL,
+	 * otherwise add the new node to beginning of the list
+	 */
+	node->next = ht->array[index];
 	ht->array[index] = node;
+
 	return (1);
 }
